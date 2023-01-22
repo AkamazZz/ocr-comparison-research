ef84de20c348879e983aa92a2e7b92f59d3e8d2e
Akezhan <akezhan.issadilov@gmail.com>
2023-01-23 00:34:22 +0600

    Bar chart added

---
+++ bar_chart.py
@@ -0,0 +1,17 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data = {'ABBYY Finereader': 72.65, 'EasyOCR': 90.28, 'TesseractOCR': 96.23}
+courses = list(data.keys())
+values = list(data.values())
+
+fig = plt.figure(figsize=(10, 5))
+
+# creating the bar plot
+plt.bar(courses, values, color='maroon',
+        width=0.4)
+
+plt.xlabel("Name of OCR engines")
+plt.ylabel("Accuracy percentage %")
+plt.title("Comparison of accuracy of OCR (Optical Character Recognition) engines")
+plt.show()
--- pie_chart.py
+++
@@ -1,7 +0,0 @@
-df = pd.DataFrame(dict(
-    group = ["A", "B", "C", "D", "E"],
-    value = [14, 12, 8, 10, 16]))
-
-fig = px.bar(df, x = 'group', y = 'value')
-
-fig.show()
